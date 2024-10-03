7824c31b194a4d9fd436a63b3be0d68b2ae647e7
Rachele Formica <racheleformica98@gmail.com>
2024-10-03 12:21:35 +0200

    done

---
+++ src/main/java/com/example/jaba/m6/s7/ex/MaxMin.java
@@ -0,0 +1,17 @@
+package com.example.jaba.m6.s7.ex;
+
+public class MaxMin {
+	public static void main(String[] args) {
+		int x = 42;
+		System.out.println("x is " + x);
+		int y = 53;
+		System.out.println("y is " + y);
+
+		int max = Math.max(x, y);
+		System.out.println("Max is " + max);
+
+		int min = Math.min(x, y);
+		System.out.println("Min is " + min);
+	}
+
+}
